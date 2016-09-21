738dd00350c383ae2fd631814a8a863b38ef18cf
Maayan Blum <maayanblm@gmail.com>
2016-09-21 18:41:24 +0300

    minor changes

--- imports/collections/games.js
+++ imports/collections/games.js
@@ -8,6 +8,7 @@ export const Games = new Meteor.Collection('games');
 //export const Test = new Meteor.Collection('test');
 
 if (Meteor.isServer) {
+  Games.remove({kifu: {$exists: false}});
 
 	Meteor.publish('playerGames', function gamesPublication() {
     if(this.userId){
